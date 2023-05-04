6d3d487547f7fef60b1e56c02bf5f357015b5f42
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-05-04 15:57:36 -0400

    Init homepage w/tabs

---
+++ src/pages/home.tsx
@@ -0,0 +1,34 @@
+import { NextPage } from "next";
+import { useState } from "react";
+
+import { api } from "~/utils/api";
+import { signIn, signOut } from "next-auth/react";
+
+interface TabProps {
+    title: string;
+    isActive: boolean; 
+    onClick: () => void;
+}
+const Tab: React.FC<TabProps> = ({ title, isActive = false, onClick }) => {
+    return (
+        <button onClick={onClick} className={isActive ? "underline underline-offset-8" : ""}>
+            {title}
+        </button>   
+    );
+};
+ 
+
+export const Home: NextPage = () => {
+    const [activeTab, setActiveTab] = useState(0);
+
+    return (
+        <main>
+            <div className="w-screen h-10 fixed top-0 left-0 flex flex-row">
+                <Tab title={"Teacher Search"} isActive={false} onClick={() => setActiveTab(0)} />
+                <Tab title={"My Messages"} isActive={false} onClick={() => setActiveTab(1)} />
+                <button onClick={() => void signOut({ callbackUrl: '/' })}>Sign out</button>
+            </div>
+            {activeTab === 0 && }
+        </main>
+    );
+};
