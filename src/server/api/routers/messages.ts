fa5c4399a1bc81665547e9597ff6da8d325e40c3
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-05-05 16:08:46 -0400

    Added sendMessage endpoint

--- src/server/api/routers/messages.ts
+++ src/server/api/routers/messages.ts
@@ -13,6 +13,27 @@ export const messagesRouter = createTRPCRouter({
       }
     });
   }),
-
-
+  
+  //TODO: sendMessage currently ONLY adds the message to the database, add content filtering and SendGrid later
+  sendMessage: protectedProcedure
+      .input(
+          z.object({
+              teacherId: z.string().cuid(),
+              content: z.string(),
+          })
+      )
+      .mutation(({ ctx, input }) => {
+        const msg = ctx.prisma.message.create({
+          data: {
+            student: {
+              connect: {id: ctx.session.user.id}
+            },
+            teacher: {
+              connect: {id: input.teacherId}
+            },
+            content: input.content
+          }
+        });
+        return msg;
+    }),
 });
