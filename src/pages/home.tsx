cdd25d2c6ba0e26281411f81928934e2fdbb97d0
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-05-06 18:04:19 -0400

    Homepage w/better HTML

--- src/pages/home.tsx
+++ src/pages/home.tsx
@@ -1,8 +1,8 @@
 import type { NextPage } from "next";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import { useRouter } from "next/router"
 
-import { api } from "~/utils/api";
-import { signIn, signOut } from "next-auth/react";
+import { signOut, useSession } from "next-auth/react";
 import { TeacherSearch } from "~/components/TeacherSearch"
 
 interface TabProps {
@@ -21,17 +21,41 @@ const Tab: React.FC<TabProps> = ({ title, isActive = false, onClick }) => {
 
 const Home: NextPage = () => {
     const [activeTab, setActiveTab] = useState(0);
+    const {data: session, status} = useSession();
+    const router = useRouter();
 
-    return (
-        <main>
-            <div className="w-screen h-10 fixed top-0 left-0 flex flex-row">
-                <Tab title={"Teacher Search"} isActive={false} onClick={() => setActiveTab(0)} />
-                <Tab title={"My Messages"} isActive={false} onClick={() => setActiveTab(1)} />
-                <button onClick={() => void signOut({ callbackUrl: '/' })}>Sign out</button>
-            </div>
-            {activeTab === 0 && <TeacherSearch />}
-        </main>
-    );
+    useEffect(() => {
+        if (status === "unauthenticated" || !session) {
+            void router.push("/");
+        }
+    });
+
+    if (status === "authenticated") {
+        return (
+            <main className="w-screen h-screen flex flex-col justify-normal">
+                <div className="w-screen h-2/6 flex flex-row space-between">
+                    <div className="font-bold">Your Dashboard</div>
+                    <div className="flex flex-col justify-evenly">
+                        <div>{session.user?.email}</div>
+                        <button className="rounded-full border" onClick={() => void signOut({ callbackUrl: '/' })}>Sign out</button>
+
+                    </div>
+                </div>
+                <div className="w-screen h-1/6 fixed top-0 left-0 flex flex-row">
+                    <Tab title={"Teacher Search"} isActive={false} onClick={() => setActiveTab(0)} />
+                    <Tab title={"My Messages"} isActive={false} onClick={() => setActiveTab(1)} />
+                </div>
+                <div>
+                    {[<TeacherSearch key={0}/>][activeTab]}
+                </div>
+            </main>
+        );
+
+    } else {
+        return (
+            <div>Loading...</div>
+        )
+    }
 };
 
 export default Home;
