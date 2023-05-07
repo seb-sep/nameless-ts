63c5176b64d4d1d0586e6d1ac489eb3a6aa39359
seb-sep <59621473+seb-sep@users.noreply.github.com>
2023-05-07 16:50:50 -0400

    Switching email to SendGrid

--- src/server/auth.ts
+++ src/server/auth.ts
@@ -65,16 +65,9 @@ export const authOptions: NextAuthOptions = {
   adapter: PrismaAdapter(prisma),
   providers: [
     EmailProvider({
-      server: {
-        host: process.env.EMAIL_SERVER_HOST,
-        port: process.env.EMAIL_SERVER_PORT,
-        auth: {
-          user: process.env.EMAIL_SERVER_USER,
-          pass: process.env.EMAIL_SERVER_PASSWORD
-        }
-      },
-      from: process.env.EMAIL_FROM,
-    }),
+      server: process.env.EMAIL_SERVER,
+      from: process.env.EMAIL_FROM
+          }),
   ],
   secret: process.env.NEXTAUTH_SECRET
 };
